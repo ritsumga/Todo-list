cf66629f74c13c61618fe239fe2504e811b2f214
ritsumga <ritsu37@icloud.com>
2023-09-24 13:13:23 +0900

    ひな形作成

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2>Todo リスト</h2>
+        <form onSubmit={() => {}}>
+          <input type="text" onChange={() => {}} className="inputText"/>
+        </form>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
